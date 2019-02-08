00fc1deb0c3ed3d4b8e13556d2c2fd30950ceec5
QuirinEcker <quirin.ecker@bajupa.com>
2019-02-08 20:51:59 +0100

    standpoint push

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,3 +1,6 @@
+let status = 0;
+setTimeout(display, 100);
+
 document.getElementById("leftButton").onClick = function() {
 	slideLeft();
 }
@@ -6,24 +9,38 @@ document.getElementById("rightButton").onClick = function() {
 	slideRight();
 }
 
+//document.html.body.addEventListener("load", display);
+
 function slideLeft() {
-	let slideShowNews = document.getElementsByClassName("slideshow-news")
-	let status = 0;
+	let slideShowNews = document.getElementsByClassName("slideshow-news");
+	status--;
 	
-	for(let i = 0; i < slideShowNews.length; i++ ) {
-		if(i == status) {
+	if(status < 0) {
+		status = slideShowNews.length - 1;
+	}
+	
+	display();
+}
+
+function slideRight() {
+	let slideShowNews = document.getElementsByClassName("slideshow-news");
+	status++;
+	
+	if(status > slideShowNews.length - 1) {
+		status = 0;
+	}
+	
+	display();
+}
+
+function display() {
+	let slideShowNews = document.getElementsByClassName("slideshow-news")
+	
+	for(let i = 0; i < slideShowNews.length; i++) {
+		if(i === status) {
 			slideShowNews[i].style.display = "block";
 		} else {
 			slideShowNews[i].style.display = "none";
 		}
 	}
-	
-	if(status == 0) {
-		status = slideShowNews.length;
-	} else {
-		status--;
-	}
-}
-
-function slideRight() {
 }
